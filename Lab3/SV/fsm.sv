f03c860ae14a175fe196345cb438c9e68d17143c
Isaiah Penny <isaiahpenny8@gmail.com>
2024-03-15 13:11:25 -0500

    Need to work on conditional statements within states

--- Lab3/SV/fsm.sv
+++ Lab3/SV/fsm.sv
@@ -1,8 +1,9 @@
-module FSM (clk, reset, a, y);
+module FSM (clk, reset, L, R, y);
 
    input logic  clk;
    input logic  reset;
-   input logic 	a;
+   input logic 	L;
+   input logic R;
    
    output logic y;
 
@@ -18,24 +19,70 @@ module FSM (clk, reset, a, y);
    always_comb
      case (state)
        S0: begin
-	  y <= 1'b0;	  
-	  if (a) nextstate <= S0;
+	  y <= 6'b000_000;	  
+	  if (R) nextstate <= S4;
+    else if (L&&R) nextstate <= S7;
 	  else   nextstate <= S1;
        end
+
+    //Left Turn Signal     
        S1: begin
-	  y <= 1'b0;	  	  
-	  if (a) nextstate <= S2;
-	  else   nextstate <= S1;
+	  y <= 6'b001_000;	  	  
+	  if (R) nextstate <= S2;
+    else if (L)
+    else if(L&&R)
+	  else   nextstate <= S2;
        end
        S2: begin
-	  y <= 1'b1;	  	  
+	  y <= 6'b011_000;	  	  
+	  if (a) nextstate <= S2;
+	  else   nextstate <= S3;
+       end 	  
+	  nextstate <= S0;
+        S3: begin
+	  y <= 6'b111_000;	  	  
+	  if (a) nextstate <= S2;
+    if(L) nextstate <= 
+	  else   nextstate <= S0;
+       end
+    
+    //Right Turn Signal
+        S4: begin
+	  y <= 6'b000_100;	  	  
+	  if (a) nextstate <= S2;
+	  else   nextstate <= S5;
+       end
+        S5: begin
+	  y <= 6'b000_110;	  	  
+	  if (a) nextstate <= S2;
+	  else   nextstate <= S6;
+       end
+        S6: begin
+	  y <= 6'b011_111;	  	  
+	  if (a) nextstate <= S2;
+	  else   nextstate <= S7;
+       end
+
+    //Hazard Signals
+        S7: begin
+	  y <= 6'b001_100;	  	  
+	  if (a) nextstate <= S2;
+	  else   nextstate <= S8;
+       end
+        S8: begin
+	  y <= 6'b011_110;	  	  
+	  if (a) nextstate <= S2;
+	  else   nextstate <= S9;
+       end
+        S9: begin
+	  y <= 6'b111_111;	  	  
 	  if (a) nextstate <= S2;
 	  else   nextstate <= S0;
        end
        default: begin
-	  y <= 1'b0;	  	  
-	  nextstate <= S0;
-       end
+	  y <= 6'000_000;
+       end	 
+       
      endcase
    
 endmodule
